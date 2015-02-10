a9d8d9e014500cd084026cac2cf1916779b58233
duXor <kontakt@dusanperisic.com>
2015-02-11 00:25:39 +0100

    Google toolbar
    Rijesen google toolbar - vise ne postoji, jedino ostaje slika srpske
    zastave ali nema veze.

--- public/php/dugme-prevodioca.php
+++ public/php/dugme-prevodioca.php
@@ -10,3 +10,7 @@ function googleTranslateElementInit() {
 }
 </script>
 <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+<style>
+	.goog-te-banner-frame.skiptranslate {display: none !important;}
+	body { top: 0px !important; }
+</style>
