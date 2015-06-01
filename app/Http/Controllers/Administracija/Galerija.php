5cddda19f8e0c6036bd6a95814f9a7821d1b5ce8
duxor <dullle@live.com>
2015-06-01 11:27:48 +0200

    A 3.3.1
    EDIT GALERIJE

--- app/Http/Controllers/Administracija/Galerija.php
+++ app/Http/Controllers/Administracija/Galerija.php
@@ -1,6 +1,7 @@
 <?php namespace App\Http\Controllers\Administracija;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Korisnici;
 use App\Sadrzaj;
 use App\Security;
 use App\OsnovneMetode;
@@ -36,7 +37,6 @@ class Galerija extends Controller {
 			fpassthru($stream);
 		}, $response_code, $headers);
 	}
-
 	//Pregled svih galerija
 	public function getIndex(){
 		if(!Security::autentifikacijaTest(4,'min')) return Security::rediectToLogin();
@@ -47,9 +47,9 @@ class Galerija extends Controller {
 		if(!Security::autentifikacijaTest(4,'min')) return Security::rediectToLogin();
 		return json_encode(['foto'=>OsnovneMetode::listaFotografija(Input::get('folder')),'video'=>OsnovneMetode::listaFajlovaSamoIme(Input::get('folder'))]);
 	}
-	public function postUkloniFoto($slugApp){
+	public function postUkloniFoto(){
 		if(!Security::autentifikacijaTest(2,'min'))return Security::rediectToLogin();
-		if(!Nalog::where('slug',$slugApp)->where('korisnici_id',Session::get('id'))->get())return Security::rediectToLogin();
+		if(!Korisnici::find(Session::get('id')))return Security::rediectToLogin();
 		return json_encode(['msg'=>(unlink(Input::get('link')))?'OK':'GRESKA']);
 	}
 	//Upload fotografija
@@ -91,53 +91,10 @@ class Galerija extends Controller {
 		echo json_encode($output);
 		return;
 	}
-
-
-
-/*
-	//Pregled svih galerija
-	public function getIndex_x($slugApp){
-		if(!Security::autentifikacijaTest(4)) return Security::rediectToLogin();
-		if(!Nalog::where('slug',$slugApp)->where('korisnici_id',Session::get('id'))->get(['id'])->first())return'Greska!';
-		$podaci['galerije']=Sadrzaji::join('nalog','nalog.id','=','sadrzaji.nalog_id')
-			->join('templejt','templejt.id','=','sadrzaji.templejt_id')
-			->join('tema','tema.id','=','nalog.tema_id')
-			->where('nalog.aktivan',1)
-			->where('nalog.korisnici_id',Session::get('id'))
-			->whereBetween('templejt.vrsta_sadrzaja_id',[7,9])
-			->get(['nalog.naziv as app','sadrzaji.id','tema.slug as slugTema','sadrzaji.naziv','templejt.slug','sadrzaj','templejt.vrsta_sadrzaja_id'])->toArray();
-		foreach($podaci['galerije'] as $k => $galerija)
-			$podaci['galerije'][$k]['slike'] = OsnovneMetode::listaFotografija(OsnovneMetode::folderGalerije($galerija['vrsta_sadrzaja_id'],$galerija['slug'],$slugApp,$galerija['slugTema']));
-
-		$podaci['aplikacije']=OsnovneMetode::aplikacije();
-		$podaci['aplikacija']=$slugApp;
-		return Security::autentifikacija('moderacija.galerije.pregled', compact('podaci'));
-	}
-	//Pregled odredjene galerije
-	public function anyGalerija($slugApp,$sadrzajId,$slugGalerije=null){
-		if (!Security::autentifikacijaTest(4))return Security::rediectToLogin();
-		$podaci['aplikacije']=OsnovneMetode::aplikacije();
-		$podaci['galerija']=Sadrzaji::join('templejt','templejt.id','=','sadrzaji.templejt_id')
-			->join('tema','tema.id','=','templejt.tema_id')
-			->where('sadrzaji.id',$sadrzajId)->get(['templejt.slug','sadrzaji.id','sadrzaji.naziv','sadrzaj','vrsta_sadrzaja_id','tema.slug as slugTema'])->first()->toArray();
-		$podaci['folder']=OsnovneMetode::folderGalerije($podaci['galerija']['vrsta_sadrzaja_id'],$podaci['galerija']['slug'],$slugApp,$podaci['galerija']['slugTema']);
-		$podaci['slike'] =OsnovneMetode::listaFotografija($podaci['folder']);
-		$podaci['slugApp']=$slugApp;
-		return Security::autentifikacija('moderacija.galerije.galerija', compact('podaci'));
-	}
-	//Fizicko BRISANJE odredjene fotografije iz galerije
-	public function postUkloniFoto_OLD($slugApp){
-		if(!Security::autentifikacijaTest())return Security::rediectToLogin();
-		if(!Nalog::where('slug',$slugApp)->where('korisnici_id',Session::get('id'))->get())return Security::rediectToLogin();
-		unlink(Input::get('slika'));
-		return Redirect::back();
-	}*/
 	//Popuna podataka o odredjenoj galeriji
 	public function postGalerijaUpdate(){
-		$podaci=json_decode(Input::get('podaci'));
-		if(!Security::autentifikacijaTest(4) or Session::get('id')!=$podaci->korisnik_id or !Sadrzaji::join('nalog','nalog.id','=','sadrzaji.nalog_id')->where('nalog.korisnici_id',Session::get('id'))->where('sadrzaji.id',$podaci->galerija_id)->get(['sadrzaji.id']))
-			return json_encode(['msg'=>'Greska!!! Proverite podatke i obratite pažnju na legalnost onoga što radite.','check'=>0]);
-		$galerija=Sadrzaji::where('id',$podaci->galerija_id)->update(['naziv'=>$podaci->naziv,'sadrzaj'=>$podaci->sadrzaj]);//find($podaci->galerija_id,['id','naziv','sadrzaj']);
-		return json_encode(['msg'=>$galerija?'Uspešno ste ažurirali galeriju fotografija.':'Desila se greška. Proverite podatke i pokušjte ponovo.','check'=>$galerija?1:0]);
+		if(!Security::autentifikacijaTest(4,'min'))Security::rediectToLogin();
+		Sadrzaj::where('slug',Input::get('slug'))->update(['sadrzaj'=>Input::get('tekst')]);
+		return'OK'.Input::get('slug').Input::get('tekst');
 	}
 }
