2d4d034fb35931b094b2436139fed3e4b65f07ab
duxor <dullle@live.com>
2015-06-23 22:53:23 +0200

    A 3.6

--- resources/views/pocetna.blade.php
+++ resources/views/pocetna.blade.php
@@ -96,48 +96,58 @@
     <div id="skrollr-body">
         {{--pocetna START::--}}
         <div id="pocetna">
-            <div class="container" style="padding-top:60px">
-                <div class="col-sm-5 jumbotron">
-                    <div class="col-sm-12">
-                        <h1>{!! $pocetnaNaslov !!}</h1>
+            <div class="container paddingTop">
+                <div class="col-sm-5">
+                    <div class="jumbotron">
+                        <div class="col-sm-12">
+                            <h1>{!! $pocetnaNaslov !!}</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            {!! $pocetnaTekst !!}
+                            <a href="#" class="scroll-link btn btn-lg btn-info" data-id="kontakt"><span class="glyphicon glyphicon-earphone"></span> Kontaktirajte nas</a>
+                        </div>
+                        <br clear="all">
                     </div>
-                    <div class="col-sm-6">
-                        {!! $pocetnaTekst !!}
-                        <a href="#" class="scroll-link btn btn-lg btn-info" data-id="kontakt"><span class="glyphicon glyphicon-earphone"></span> Kontaktirajte nas</a>
-                    </div>
+                    <div class="krug krug-fb">f</div>
+                    <div class="krug krug-default">t</div>
+                    <div class="krug krug-gpp">g+</div>
+                    <div class="krug krug-mail">@</div>
                 </div>
                 <div class="col-sm-7">
-                    <!-- < ? php require_once'php/slideshow.php'? > -->                   
-                    
-                    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                      <!-- Indicators -->
-                      <ol class="carousel-indicators">
-                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                      </ol>
+                    <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                          <!-- Indicators -->
+                          <ol class="carousel-indicators">
+                            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                          </ol>
 
-                      <!-- Wrapper for slides -->
-                      <div class="carousel-inner" role="listbox">
-                        <div class="item active">
-                          <img src="/slike/galerije/osnovni-slider/1.jpg" alt="...">
-                        </div>
-                        <div class="item">
-                          <img src="/slike/galerije/osnovni-slider/2.jpg" alt="...">
-                        </div>
-                      </div>
+                          <!-- Wrapper for slides -->
+                          <div class="carousel-inner" role="listbox">
+                            <div class="item active">
+                              <img src="/slike/galerije/osnovni-slider/1.jpg" alt="...">
+                            </div>
+                            <div class="item">
+                              <img src="/slike/galerije/osnovni-slider/2.jpg" alt="...">
+                            </div>
+                          </div>
 
-                      <!-- Controls -->
-                      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                      </a>
-                      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                      </a>
-                    </div>
-                    
+                          <!-- Controls -->
+                          <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                          </a>
+                          <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                          </a>
+                        </div>
+                        <p>
+                            <h1>Prvi Pomeranac u Srbiji</h1>
+                            Imate čast da upoznate psa o kome se toliko priča i koji je prvi Pomeranac u Srbiji, a predstavlja izuzetno retku rasu, poreklom iz...</p>
+                        </div></div>
                     
                     
                     
