52ef7fdc0195743a209de0a9139d4e2e602902d3
duxor <dullle@live.com>
2015-06-23 17:45:00 +0200

    A 3.5

--- resources/views/pocetna.blade.php
+++ resources/views/pocetna.blade.php
@@ -96,16 +96,51 @@
     <div id="skrollr-body">
         {{--pocetna START::--}}
         <div id="pocetna">
-            <div class="container">
-                <div class="col-sm-12">
-                    <h1>{!! $pocetnaNaslov !!}</h1>
+            <div class="container" style="padding-top:60px">
+                <div class="col-sm-5 jumbotron">
+                    <div class="col-sm-12">
+                        <h1>{!! $pocetnaNaslov !!}</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        {!! $pocetnaTekst !!}
+                        <a href="#" class="scroll-link btn btn-lg btn-info" data-id="kontakt"><span class="glyphicon glyphicon-earphone"></span> Kontaktirajte nas</a>
+                    </div>
                 </div>
-                <div class="col-sm-6">
-                    {!! $pocetnaTekst !!}
-                    <a href="#" class="scroll-link btn btn-lg btn-info" data-id="kontakt"><span class="glyphicon glyphicon-earphone"></span> Kontaktirajte nas</a>
-                </div>
-                <div class="col-sm-6">
-                    <?php require_once'php/slideshow.php'?>
+                <div class="col-sm-7">
+                    <!-- < ? php require_once'php/slideshow.php'? > -->                   
+                    
+                    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                      <!-- Indicators -->
+                      <ol class="carousel-indicators">
+                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                      </ol>
+
+                      <!-- Wrapper for slides -->
+                      <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                          <img src="/slike/galerije/osnovni-slider/1.jpg" alt="...">
+                        </div>
+                        <div class="item">
+                          <img src="/slike/galerije/osnovni-slider/2.jpg" alt="...">
+                        </div>
+                      </div>
+
+                      <!-- Controls -->
+                      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                      </a>
+                      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                      </a>
+                    </div>
+                    
+                    
+                    
+                    
                 </div>
                 <div id="scrollToTop" style="position:fixed;bottom:20px;right:20px"><button class="scroll-link btn btn-lg btn-info" data-id="top"><i class="glyphicon glyphicon-chevron-up"></i></button></div>
     <script>
