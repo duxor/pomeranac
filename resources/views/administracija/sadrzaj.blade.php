2dee2b468a92e723f0846514889468ba3c7fff56
Saša Jović <jovicsasajovic@gmail.com>
2015-06-28 18:16:57 +0200

    sadrzaj

--- resources/views/administracija/sadrzaj.blade.php
+++ resources/views/administracija/sadrzaj.blade.php
@@ -41,7 +41,7 @@
                 </div>
             </div>
             <div class="form-group">
-            {!! Form::button('<span class="glyphicon glyphicon-envelope"></span> Pošalji',['id'=>'salji', 'class'=>'btn btn-lg btn-primary','onclick'=>'Komunikacija.posalji("/admin/sadrzaji/sadrzaj",\'zaSlanje\',\'poruka\',\'wait\',\'asd\')' ]) !!}  
+            {!! Form::button('<span class="glyphicon glyphicon-envelope"></span> Pošalji',['id'=>'salji', 'class'=>'btn btn-lg btn-primary']) !!}  
             </div>
         </div>
     </div>
@@ -74,6 +74,7 @@ $(document).ready(function() {
 
    $('#salji').click(function() {
     $('textarea:tinymce').tinymce().save();
+    Komunikacija.posalji("/admin/sadrzaji/sadrzaj","zaSlanje","poruka","wait","asd");
 });
 
 });  
@@ -102,87 +103,6 @@ $(document).ready(function() {
     }
 </script>
 
-
-
-
-{{--
-    <script>
-        $(document).ready(function(){
-            switch('{{$podaci['akcija']}}'){
-                case'novi':kreirajNovi();break;
-                case'pregled':getPregled();break;
-            }
-        });
-        function setActive(ID){
-            $('.active').removeClass('active');
-            $('#'+ID).addClass('active');
-        }
-        function kreirajNovi(){
-            setActive('novi');
-            $('#show').hide();
-            $('#wait').show();
-            $('#show').html(
-                    '<div id="zaSlanje" class="form-horizontal">'+
-                    '{!!Form::hidden('_token',csrf_token())!!}'+
-            '<div class="form-group">'+
-            '{!!Form::select('app',$podaci['app'],null,['class'=>'form-control'])!!}'+
-            '</div>'+
-            '<div class="form-group">'+
-            '<input name="naslov" class="form-control" placeholder="Naslov">'+
-            '</div>'+
-            '<div class="form-group">'+
-            '<textarea name="tekst" class="form-control" placeholder="Objava..." rows="7"></textarea>'+
-            '</div>'+
-            '<div class="form-group">'+
-            '<button class="btn btn-lg btn-primary" onclick="Komunikacija.posalji(\'/{{\App\OsnovneMetode::osnovniNav()}}/blog/dodaj-objavu\',\'zaSlanje\',\'poruka\',\'wait\',\'show\')"><i class="glyphicon glyphicon-envelope"></i> PoÅ¡alji</div>'+
-            '</div>'+
-            '</div>');
-            $('#wait').hide();
-            $('#show').fadeIn();
-        }
-        function getPregled(){
-            setActive('pregled');
-            $('#show').hide();
-            $('#wait').show();
-            $.post('/{{\App\OsnovneMetode::osnovniNav()}}/blog/pregled/{{Session::get('username')}}',{
-                _token:'{{csrf_token()}}'
-            },function(data){
-                if(data) {
-                    var podaci = JSON.parse(data);
-                    if (podaci.length) {
-                        var objave = '';
-                        for (var i = 0; i < podaci.length; i++)
-                            objave += '<div onclick="getPoruka(\'' + podaci[i].naslov + '\',\'poslate\')" class="citajPoruku">'+
-                                        '<div class="nDn col-sm-3">Naslov</div><div class="col-sm-9">' + podaci[i].naslov +
-                                        '</div><div class="nDn col-sm-3">Objava</div><div class="col-sm-9">' + podaci[i].tekst +
-                                        '</div><div class="nDn col-sm-3">Aktivan</div><div class="col-sm-9">' + podaci[i].aktivan + '</div></div>';
-                        $('#show').html(objave);
-                    } else $('#show').html('<p>Nemate ni jednu objavu.</p>');
-                } else $('#show').html('<p>GreÅ¡ka u autentifikaciji. Kontaktirajte administratora.</p>');
-                $('#wait').hide();
-                $('#show').fadeIn();
-            });
-        }
-    </script>
-    <script type="text/javascript">
-        tinymce.init({
-            selector: "textarea",
-            theme: "modern",
-            plugins: [
-                "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-                "searchreplace wordcount visualblocks visualchars code fullscreen",
-                "insertdatetime media nonbreaking save table contextmenu directionality",
-                "emoticons template paste textcolor colorpicker textpattern"
-            ],
-            toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
-            toolbar2: "print preview media | forecolor backcolor emoticons",
-            image_advtab: true,
-            templates: [
-                {title: 'Test template 1', content: 'Test 1'},
-                {title: 'Test template 2', content: 'Test 2'}
-            ]
-        });
-</script>--}}
 @endsection
 @section('body')
 
